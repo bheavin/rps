70f0a697e4f8cd3766db4a035173458fedf9b6a7
Christopher <bheavin2001@gmail.com>
2022-06-25 06:27:18 -0500

    created rps

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+
+const choices = ['rock', 'paper', 'scissors'];
+let computerChoice;
+
 var button = document.getElementById('myButton');
-function promptMe() {
-    const playerChoice = prompt("Choose 'Rock', 'Paper', or 'Scissors'");
-}
+function promptMe(button) {
+    computerChoice = choices[Math.floor(Math.random()*choices.length)];
+
+    const playerChoice = prompt("Choose 'Rock', 'Paper', or 'Scissors'").toLowerCase();
+    if (playerChoice === computerChoice) {
+        bootbox.alert(`You chose ${playerChoice}, and the computer chose ${computerChoice}, so it's a draw!`);
+    } if (playerChoice === 'rock' && computerChoice === choices[1]) {
+        alert(`You chose ${playerChoice}, the computer chose ${computerChoice}, so you lose.`);
+    } if (playerChoice === 'rock' && computerChoice === choices[2]) {
+        alert(`You chose ${playerChoice}, the computer chose ${computerChoice}, so you win!`);
+    } 
+    if (playerChoice === 'paper' && computerChoice === choices[2]) {
+        alert(`You chose ${playerChoice}, the computer chose ${computerChoice}, so you lose.`);
+    } if (playerChoice === 'paper' && computerChoice === choices[0]) {
+        alert(`You chose ${playerChoice}, the computer chose ${computerChoice}, so you win!`);
+    } 
+    if (playerChoice === 'scissors' && computerChoice === choices[0]) {
+        alert(`You chose ${playerChoice}, the computer chose ${computerChoice}, so you lose.`);
+    } if (playerChoice === 'scissors' && computerChoice === choices[1]) {
+        alert(`You chose ${playerChoice}, the computer chose ${computerChoice}, so you win!`);
+    } 
+};
